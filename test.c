c39773a9eeab718e4b5c305cf79c5abdbe91726f
Pauline VAUDON <pavaudon@e1r10p7.42.fr>
2018-09-25 18:34:35 +0200

    test a finir

--- test.c
+++ test.c
@@ -1,92 +1,60 @@
-#include "libft.h"
+#include "test.h"
 
-int		ft_is_in_tab(char **tab, int value)
+int		ft_stack_sort(t_stack *stack)
 {
-	while (tab)
+	t_stack *tmp;
+
+	tmp = stack;
+	while (tmp->next)
 	{
-		if (value == ft_atoi(*tab))
+		if (tmp->value > tmp->next->value)
 			return (0);
-		tab++;
+		tmp = tmp->next;
 	}
 	return (1);
 }
 
-int		ft_strtablen(char **tab)
+void 	ft_print(t_stack *stack)
+{
+	ft_simple_printf("STACK :\n");
+	while (stack->next)
+	{
+		ft_simple_printf("%d\t", stack->value);
+		stack = stack->next;
+	}
+}
+
+int 	main(int argc, char **argv)
 {
 	int i;
+	t_stack *stack;
+	t_stack *begin;
 
 	i = 0;
-	while (tab[i])
-		i++;
-	return (i);
-}
-
-int		ft_again_in_tab(char **tab, int *j, int value, int len)
-{
-	if (*j < (len - 1))
+	if (argc > 1)
 	{
-		if (!(ft_is_in_tab(tab + 1, value)))
+		if (!(stack = (t_stack*)ft_memalloc(sizeof(t_stack))))
 			return (0);
-		*j += 1;
+		ft_simple_printf("argc : '%d'\n", argc);
+		while (++i < argc)
+		{
+			ft_simple_printf("coucou1\n");
+			ft_simple_printf("argv[%d] : '%s' : '%d'\n", i, argv[i], ft_atoi(argv[i]));
+			stack->value = ft_atoi(argv[i]);
+			ft_simple_printf("value : '%d'\n", stack->value);
+			stack->next = NULL;
+			if (i == 1)
+				begin = stack;
+			ft_simple_printf("coucou2\n");
+			stack = stack->next;
+			ft_simple_printf("coucou3\n");
+		}
+		ft_simple_printf("coucou4\n");
+		ft_print(begin);
+		if (!ft_stack_sort(begin))
+			ft_simple_printf("KOOOOO\n");
+		else
+			ft_simple_printf("OK GOOOOD\n");
 	}
-	else
-	{
-		*j = 0;
-		free(tab);
-	}
-	return (1);
-}
-
-char	**ft_filltabvalue(char *str, int *len)
-{
-	char **tab;
-
-	tab = ft_strsplit(str, ' ');
-	*len = ft_strtablen(tab);
-	return (tab);
-}
-
-int		ft_is_again(char **argv, int argc, int value)
-{
-	int		i;
-	int		j;
-	int		len;
-    int     tmp;
-	char	**tab;
-
-	i = 0;
-	j = 0;
-	while (++i < argc - 1)
-	{
-		if (ft_strchr(argv[i], ' ') && !tab)
-			tab = ft_filltabvalue(argv[i], &len);
-		value = !tab ? ft_atoi(argv[i]) : ft_atoi(tab[j]);
-        tmp = i;
-        while (++tmp < argc - 1)
-        {
-            if (tab && j < len && !ft_again_in_tab(tab, &j, value, len))
-                return (0);
-            else if (ft_strchr(argv[tmp + 1], ' ') &&
-            !ft_is_in_tab(ft_strsplit(argv[tmp + 1], ' '), value))
-                return (0);
-            else
-            {
-                if (value == ft_atoi(argv[tmp + 1]))
-                    return (0);
-            }
-        }
-    }
-	return (1);
-}
-
-int     main(int argc, char **argv)
-{
-    if (argc > 1)
-    {
-        if (ft_is_again(argv, argc, 0))
-            write(1, "OK\n", 3);
-        else
-            write(1, "NO\n", 3);
-    }
-    return (0);
+	return (0);
 }
--- test.h
+++ test.h
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_new_check_data.c                                :+:      :+:    :+:   */
+/*   test.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pavaudon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/20 12:59:53 by pavaudon          #+#    #+#             */
-/*   Updated: 2018/09/20 12:59:54 by pavaudon         ###   ########.fr       */
+/*   Created: 2018/09/25 15:50:32 by pavaudon          #+#    #+#             */
+/*   Updated: 2018/09/25 15:50:33 by pavaudon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#ifndef TEST_H
+# define TEST_H
+
+# include <stdio.h>
+
+# include "libft.h"
+
+typedef struct		s_stack
+{
+    int			      	value;
+    struct s_stack	*next;
+}               	t_stack;
+
+#endif
