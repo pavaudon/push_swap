f85a13a76cd7e729a72630082857306c9e355ca9
Pauline VAUDON <pavaudon@e1r10p16.42.fr>
2018-10-04 13:21:49 +0200

    all command ok reste a checker is again

--- test.c
+++ test.c
@@ -1,11 +1,37 @@
 #include "test.h"
 
+int		*ft_test(int *tab, int *to_add, int tablen, int to_addlen)
+{
+	int *new;
+
+	ft_simple_printf("tabfsjoin\n");
+	if (!(new = (int*)ft_memalloc(sizeof(int) * (tablen + to_addlen))))
+		return (NULL);
+	ft_simple_printf("after new malloc\n");
+	ft_tabcpy(tab, new, tablen);
+	ft_simple_printf("after cpy tab in new\n");
+	ft_tabcpy(to_add, new + tablen, to_addlen);
+	ft_simple_printf("after cpy to_add in new\n");
+	//free(to_add);
+	ft_simple_printf("free to_add\n");
+	return (new);
+}
+
 int main()
 {
-	char *line;
+	int *tab;
+	int value;
+	int tablen;
+	int i;
 
-	line = NULL;
-	while (get_next_line(0, &line))
-		ft_simple_printf("LINE : '%s'\n", line);
+	tablen = 4;
+	if (!(tab = (int*)ft_memalloc(sizeof(int) * tablen)))
+		return (0);
+	i = -1;
+	while (++i < tablen)
+		tab[i] = 10;
+	value = 5;
+	ft_test(tab, &value, tablen, 1);
+	ft_simple_printf("after tabjsjoin\n");
 	return (1);
 }
